41b48ad100349f076c3944227d848482bd15cb1c
Jaeha Lee <jaeha0183@gmail.com>
2024-01-27 22:16:44 +0900

    FIx : TypeORM
    Dependency 문제 해결
    연결 X

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      // forRoot -> forRootAsync로 바뀌었어요!
-      useClass: TypeOrmConfigService, // DB 관련 설정이 있는 서비스 파일을 불러와요!
+      imports: [ConfigModule],
+      // forRoot -> forRootAsync
+      useClass: TypeOrmConfigService, // DB 관련 설정이 있는 서비스 파일을 불러옴
+      inject: [ConfigService], // TypeOrmConfigService에서 ConfigService를 주입
     }),
     BoardModule,
   ],
